a48b73c1e7563ac9110db025119acfc381618d5a
barbabianca <ukasikcioglu@example.com>
2023-04-13 19:35:39 +0530

    latest commit

---
+++ assets/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// portfolio carousel
+$('#owl-portfolio').owlCarousel({
+    margin:30,
+    dots: false,
+    responsiveClass:true,
+    autoplay: true, // enable automatic slide
+    autoplayTimeout: 1000, // set automatic slide timeout to 5 seconds
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+});
+
+// testmonial carousel
+$('#owl-testmonial').owlCarousel({
+    center: true,
+    items:1,
+    loop:true,
+    nav: true,
+    dots: false
+})
+
+// select the read-more link and hidden paragraph
+const readMoreLink = $('#about .read-more');
+const hiddenParagraph = $('#about p');
+
+// add a click event listener to the read-more link
+readMoreLink.on('click', function(event) {
+  // prevent the default link behavior
+  event.preventDefault();
+
+  // remove the "d-none" class from the paragraph to show the full text
+  hiddenParagraph.removeClass('d-none');
+
+  // hide the read-more link by setting its display property to "none"
+  readMoreLink.hide();
+});
